5ed9a80dd9e6bfda0de527f7610c89374fee21a9
Eche Codes <echeeze956@gmail.com>
2026-02-08 22:10:30 +0100

    Add new components for About section and implement Services page

--- utils/routes.tsx
+++ utils/routes.tsx
@@ -6,5 +6,6 @@ type Items = {
 export const publicItems: Items[] = [
     { name: 'Home', link: '/' },
     { name: 'About', link: '/about' },
+    { name: 'Services', link: '/services' },
     { name: 'Contact', link: '/contact' },
 ]
